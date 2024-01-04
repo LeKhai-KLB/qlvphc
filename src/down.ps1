511caac1bd384fb29c18132a4e79b087a7aabaa2
Ravi Le <ravi.le@soxes.ch>
2024-01-04 11:16:16 +0700

    fix docker compose

---
+++ src/down.ps1
@@ -0,0 +1,2 @@
+docker-compose -f docker-compose.yml -f docker-compose.override.yml down
+docker network rm qlvphc-network
---
+++ src/up.ps1
@@ -0,0 +1,2 @@
+docker network create qlvphc-network
+docker-compose -f docker-compose.yml -f docker-compose.override.yml up -d
