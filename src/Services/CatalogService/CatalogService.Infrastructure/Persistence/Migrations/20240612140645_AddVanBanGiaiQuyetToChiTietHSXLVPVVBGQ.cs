c0163db26e4efddf32272c4bee0169560322ee44
Khai Phan <khaiphandev@gmail.com>
2024-06-12 21:07:47 +0700

    Add VanBanGiaiQuyet to ChiTietHSXLVPVVBGQ and open authorize

---
+++ src/Services/CatalogService/CatalogService.Infrastructure/Persistence/Migrations/20240612140645_AddVanBanGiaiQuyetToChiTietHSXLVPVVBGQ.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CatalogService.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddVanBanGiaiQuyetToChiTietHSXLVPVVBGQ : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "VanBanGiaiQuyetId",
+                table: "ChiTietHSXLVPVVBGQ",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "VanBanGiaiQuyetId",
+                table: "ChiTietHSXLVPVVBGQ");
+        }
+    }
+}
